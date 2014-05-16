43ca17567e459b33d97befe26a5cea0f8a7d403e
CoolestNerdIII <basketball4lyfe2@gmail.com>
2014-05-16 19:07:27 -0400

    Initial Commit

---
+++ Flashcard Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flashcard Challenge
+//
+//  Created by Paul Wilson on 5/16/14.
+//  Copyright (c) 2014 Paul Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
